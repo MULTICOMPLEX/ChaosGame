c548c3d4242aa2f902d639b75335bf97097e5c04
MULTICOMPLEX <thepromisedplanet@gmail.com>
2022-06-24 22:10:21 +0200

    update.

--- main.cpp
+++ main.cpp
@@ -11,14 +11,40 @@ constexpr auto HEIGHT = 900;
 
 mxws<uint32_t> RNG;
 
-std::vector<Pixel> pix(3);
+std::vector<Pixel> pix;
+
+template <typename T>
+std::vector<Pixel> setcorners_square(const T& WIDTH, const T& HEIGHT) {
+	std::vector<Pixel> pix(4);
+	pix[0].x = 0;
+	pix[0].y = 0;
+	pix[1].x = WIDTH;
+	pix[1].y = 0;
+	pix[2].x = 0;
+	pix[2].y = HEIGHT;
+	pix[3].x = WIDTH;
+	pix[3].y = HEIGHT;
+	return pix;
+}
+
+template <typename T>
+std::vector<Pixel> setcorners_triangle(const T& WIDTH, const T& HEIGHT) {
+	std::vector<Pixel> pix(3);
+	pix[0].x = WIDTH / 2;
+	pix[0].y = 0;
+	pix[1].x = 0;
+	pix[1].y = HEIGHT;
+	pix[2].x = WIDTH;
+	pix[2].y = HEIGHT;
+	return pix;
+}
 
 constexpr auto ITER = 1000000;
 
-std::vector<unsigned char> image((WIDTH+4)* (HEIGHT+4) * 4);
+std::vector<unsigned char> image((WIDTH + 1)* (HEIGHT + 1) * 4);
 template <typename im, typename I, typename col>
 void putpixel(im& image, const I& x, const I& y, const col& color);
-void encodeOneStep(const char* filename, const std::vector<unsigned char>& image, 
+void encodeOneStep(const char* filename, const std::vector<unsigned char>& image,
 	unsigned width, unsigned height);
 
 int main(int argc, char* argv[])
@@ -32,24 +58,33 @@ int main(int argc, char* argv[])
 	Pixel last = { WIDTH, HEIGHT };
 	Pixel point = {};
 
-	pix[0].x = WIDTH / 2;
-	pix[0].y = 0;
-	pix[1].x = 0;
-	pix[1].y = HEIGHT;
-	pix[2].x = WIDTH;
-	pix[2].y = HEIGHT;
+	const bool square = true;
 
+	if(square)
+		pix = setcorners_square(WIDTH, HEIGHT);
+	else 
+		pix = setcorners_triangle(WIDTH, HEIGHT);
+
+	int choice = 0, choice2 = 0;
 	for (auto i = 0; i < ITER; i++) {
 
 		surf.RectFill(point.x, point.y, 1, 1, color);
 		putpixel(image, point.x, point.y, color);
 
-		int choice = RNG(2);
+		if (square) {
+			while (choice2 == choice)
+				choice = RNG(3);
+			choice2 = choice;
+		}
+
+		else 
+			choice = RNG(2);
+	
 		auto dx = pix[choice].x - last.x;
 		auto dy = pix[choice].y - last.y;
 
-		point.x = int(round(last.x + dx / 2.));
-		point.y = int(round(last.y + dy / 2.));
+		point.x = int(round(last.x + dx / 2));
+		point.y = int(round(last.y + dy / 2));
 
 		last = point;
 
@@ -61,7 +96,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void encodeOneStep(const char* filename, const std::vector<unsigned char>& image, 
+void encodeOneStep(const char* filename, const std::vector<unsigned char>& image,
 	unsigned width, unsigned height) {
 	//Encode the image
 	unsigned error = lodepng::encode(filename, image, width, height);
@@ -71,7 +106,7 @@ void encodeOneStep(const char* filename, const std::vector<unsigned char>& image
 }
 
 template <typename im, typename I, typename col>
-void putpixel(im& image, const I& x, const I& y, const col& color) 
+void putpixel(im& image, const I& x, const I& y, const col& color)
 {
 	image[4ull * WIDTH * y + 4ull * x + 0] = color.r;
 	image[4ull * WIDTH * y + 4ull * x + 1] = color.g;
